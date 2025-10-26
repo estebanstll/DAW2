ff964423a72179a2c8624ebe764b4a3c0784dc8d
estebanstll <160126678+estebanstll@users.noreply.github.com>
2025-10-26 13:42:42 +0100

    Ejercicio básico Javascript y reestructuracion de las carpetas
    Added a basic factorial calculator with HTML, CSS, and JavaScript. Also reorganized and renamed several JavaScript exercise files into new directories for better structure and clarity.

---
+++ Desarrollo Web en Entorno Cliente/Introducción a JavaScript/Ejercicio Javascript básico/factorial.js
@@ -0,0 +1,19 @@
+const numeroIntroducido = document.getElementById("factorial");
+const boton = document.getElementById("calcular");
+const output = document.getElementById("resultado");
+
+boton.addEventListener("click", () => {
+  if (parseInt(numeroIntroducido.value) > 0) {
+    let resultado = 1;
+
+    for (let index = parseInt(numeroIntroducido.value); index > 0; index--) {
+      resultado = resultado * index;
+    }
+    output.textContent = `El factorial de ${numeroIntroducido.value} es ${resultado}`;
+    output.style.color = "green";
+  } else {
+    output.textContent = "introduce un numero permitido";
+    output.style.color = "red";
+    numeroIntroducido.textContent = "";
+  }
+});
