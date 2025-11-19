3cc12eb66e2f3ed5798e70a870c511e9e930867d
estebanstll <estebansantolallar@gmail.com>
2025-11-19 17:46:04 +0100

    apuntes css
    apuntes y trabajo js

---
+++ Desarrollo Web en Entorno Cliente/Eventos y Formularios/atrapaLaPlabara/js/login.js
@@ -0,0 +1,48 @@
+// Comprobamos si ya existe un usuario almacenado
+const existingUser = localStorage.getItem("user");
+const existingPass = localStorage.getItem("password");
+
+const title = document.getElementById("title");
+const btn = document.getElementById("btn");
+const form = document.getElementById("form");
+
+if (!existingUser || !existingPass) {
+  title.textContent = "Registro inicial";
+  btn.textContent = "Registrarse";
+  btn.classList.add("h2");
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = document.getElementById("user").value;
+    const pass = document.getElementById("password").value;
+
+    localStorage.setItem("user", user);
+    localStorage.setItem("password", pass);
+
+    location.reload();
+  });
+} else {
+  title.textContent = "Iniciar sesión";
+  btn.textContent = "Entrar";
+
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    const user = document.getElementById("user").value;
+    const pass = document.getElementById("password").value;
+
+    if (user === existingUser && pass === existingPass) {
+      window.location.href = "juego.html";
+    } else {
+      document
+        .getElementById("password")
+        .setCustomValidity("Credenciales incorrectas");
+      document.getElementById("password").reportValidity();
+
+      document.getElementById("password").addEventListener("input", () => {
+        document.getElementById("password").setCustomValidity("");
+      });
+    }
+  });
+}
